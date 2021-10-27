2bd86fc6d3169a9d4b43d02ba7132ed61e7d2b35
nsalinas350 <nsalinas@unal.edu.co>
2021-10-26 19:17:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ class Auto:
 
 
 class Motor:
-    def _init_(self, numeroCilindros, tipo, registro):
+    def __init__(self, numeroCilindros, tipo, registro):
         self.numeroCilindros = numeroCilindros
         self.tipo = tipo
         self.registro = registro
